81e6d3e4116f54f63f1c5e7e08ab38ab506a253c
Mario Vellandi <mvellandi@hotmail.com>
2022-08-23 17:07:21 +0200

    updated status to unused

--- schemas/domains/project/sectionTemplate.js
+++ schemas/domains/project/sectionTemplate.js
@@ -1,18 +1,19 @@
-export default [
-  {
-    _type: "projectTopic",
-    heading: "Summary",
-  },
-  {
-    _type: "projectStructure",
-    heading: "Tech / Design",
-  },
-  {
-    _type: "projectTopic",
-    heading: "Process",
-  },
-  {
-    _type: "projectTopic",
-    heading: "Project Links",
-  },
-];
+// currently unused
+// export default [
+//   {
+//     _type: "projectTopic",
+//     heading: "Summary",
+//   },
+//   {
+//     _type: "projectStructure",
+//     heading: "Tech / Design",
+//   },
+//   {
+//     _type: "projectTopic",
+//     heading: "Process",
+//   },
+//   {
+//     _type: "projectTopic",
+//     heading: "Project Links",
+//   },
+// ];
