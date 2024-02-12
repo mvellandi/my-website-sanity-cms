edb4cbe8a5653c1d2f834a4e08f633c0dcf13e11
Mario Vellandi <mvellandi@hotmail.com>
2024-02-12 10:31:40 +0100

    - remove commented code

--- deskUtils.js
+++ deskUtils.js
@@ -1,38 +1,20 @@
-export function parentList(S, {
-  parentTitle,
-  childTitle,
-  childItems,
-  icon,
-  showChildIcons,
-}) {
+export function parentList(S, {parentTitle, childTitle, childItems, icon, showChildIcons}) {
   return S.listItem()
     .title(parentTitle)
     .icon(icon)
-    .child(
-      S.list().title(childTitle).showIcons(showChildIcons).items(childItems)
-    );
+    .child(S.list().title(childTitle).showIcons(showChildIcons).items(childItems))
 }
 
 // singleton, ex: website settings
-export function singleChildItem(S, { name, title }) {
-  return S.listItem()
-    .title(title)
-    .child(S.document().schemaType(name).documentId(name));
+export function singleChildItem(S, {name, title}) {
+  return S.listItem().title(title).child(S.document().schemaType(name).documentId(name))
 }
 
 // multi-record document, ex: project, article, page, author
-export function multiChildItem(S, { name, title, icon }) {
-  return S.listItem().title(title).icon(icon).child(S.documentTypeList(name));
+export function multiChildItem(S, {name, title, icon}) {
+  return S.listItem().title(title).icon(icon).child(S.documentTypeList(name))
 }
 
 export function listNames(list) {
-  return list.map((item) => item.name);
+  return list.map((item) => item.name)
 }
-
-// export function filterDocuments(list) {
-//   let exclusionList = [];
-//   list.forEach((sublist) => exclusionList.push(...listNames(sublist)));
-//   return S.documentTypeListItems().filter(
-//     (listItem) => !exclusionList.includes(listItem.getId())
-//   );
-// }
--- schemas/domains/project/utilities.js
+++ schemas/domains/project/utilities.js
@@ -1,37 +1,21 @@
 export function entriesUnique(values, _context) {
   // if there are duplicate entries
-  const errorMsg = "Please remove duplicate entries";
+  const errorMsg = 'Please remove duplicate entries'
   if (values) {
     // if entries are NOT references types
-    if (values[0]._type !== "reference") {
-      const entries = values.map((v) => v._type);
+    if (values[0]._type !== 'reference') {
+      const entries = values.map((v) => v._type)
       if (new Set(entries).size !== values.length) {
-        return errorMsg;
+        return errorMsg
       }
     }
     // if entries are reference types
-    else if (values[0]._type === "reference") {
-      const refs = values.map((v) => v._ref);
+    else if (values[0]._type === 'reference') {
+      const refs = values.map((v) => v._ref)
       if (new Set(refs).size !== values.length) {
-        return errorMsg;
+        return errorMsg
       }
     }
   }
-  return true;
+  return true
 }
-// unused function to combine validation functions while providing a closure around function args.
-// function buildEntriesValidationRule(validators) {
-//   return function validateEntries(Rule) {
-//     return Rule.custom((values, context) => {
-//       let errorMsg;
-//       for (const v of validators) {
-//         const error = v({ values, context });
-//         if (error) {
-//           errorMsg = error;
-//           break;
-//         }
-//       }
-//       return errorMsg ? errorMsg : true;
-//     });
-//   };
-// }
